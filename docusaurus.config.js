7b8582a1ef64e0a24efeb1bf1e005f317c7a675a
Valentina A <valentina.aktanko@local-internet.ru>
2026-02-11 20:07:29 +0300

    change base url for gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://freya27665.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/happy-valentines-day-2026/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
