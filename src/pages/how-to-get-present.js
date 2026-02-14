937ff18f4992c88a7fddf87677f34ef720da911d
Valentina A <valentina.aktanko@local-internet.ru>
2026-02-15 00:41:20 +0300

    add new page mockup

---
+++ src/pages/how-to-get-present.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function HowToGetPresent() {
+  return (
+    <Layout>
+      <h1>How to get a present</h1>
+      <p>This is a React page</p>
+    </Layout>
+  );
+}
