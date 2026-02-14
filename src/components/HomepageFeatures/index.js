381cbf6e3bf66430c547df7300fdf6abd77d5af2
Valentina A <valentina.aktanko@local-internet.ru>
2026-02-15 00:00:02 +0300

    add fox and squirrel

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: '',
-    Svg: require('@site/static/img/heart.svg').default,
+    Svg: require('@site/static/img/fox-svgrepo-com.svg').default,
     description: (
       <>
         Даже если я когда-то<br/>
@@ -29,7 +29,7 @@ const FeatureList = [
   },
   {
     title: '',
-    Svg: require('@site/static/img/fox-svgrepo-com.svg').default,
+    Svg: require('@site/static/img/squirrel-svgrepo-com.svg').default,
     description: (
       <>
         На Земле, такой огромной,<br/>
